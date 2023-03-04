6eb59a5b82af64d482811044f50fee16ecd2f74f
dotrannhattuong <tuongdotn@gmail.com>
2023-03-04 11:31:43 +0700

    Upload communicate with STM32

---
+++ utils/ser.py
@@ -0,0 +1,35 @@
+import serial
+import numpy as np
+
+class Communication:
+    def __init__(self, port="/dev/ttyACM0", baudrate=115200):
+        self.stm32 = serial.Serial(port, baudrate, timeout=0.1)
+    
+    def __call__(self, speed=100, angle=0):
+        ############ Receive DATA ############
+        # data = ser.readline().decode('utf-8')
+        # print(f"data: {ser.readline().decode('utf-8')}")
+
+        ############ SEND DATA ############
+        ###### Encode ######
+        send_0 = (np.uint16(speed) & 255).astype(np.uint8)
+        send_1 = (np.uint16(speed) >> 8).astype(np.uint8)
+        send_2 = np.uint8(angle)
+
+        send_data = [send_0, send_1, send_2]
+        self.stm32.write(send_data)
+
+        ###### Decode - DEBUG ######
+        # print(f"Speed: {np.int16(send_1<<8)|send_0}")
+        # print(f"Angle: {np.int8(send_2)}")
+
+if __name__ == "__main__":
+    stm32 = Communication()
+
+    while True:
+        stm32(speed=100, angle=0)
+
+
+
+
+    
