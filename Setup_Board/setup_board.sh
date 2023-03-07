b58e794f08256d2aeaf27afec59e8ec71274e868
dotrannhattuong <tuongdotn@gmail.com>
2023-03-07 11:04:06 +0700

    update setup board

--- Setup_Board/setup_board.sh
+++ Setup_Board/setup_board.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt upgrade
+sudo apt install nano
 sudo apt install python3-pip
 sudo apt install python3-pil
 sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran libopenblas-dev
@@ -20,6 +21,7 @@ wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -
 sudo apt-get install python3-pip libopenblas-base libopenmpi-dev libomp-dev
 pip3 install Cython
 pip3 install numpy torch-1.8.0-cp36-cp36m-linux_aarch64.whl
+sudo apt install libopenblas-base libopenmpi-dev
 
 # torchvision
 sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
